78b510819d642dfba7299cafecf63f75cf2f6715
Thibault <t.dardinier@gmail.com>
2019-04-13 17:21:01 +0200

    1. (a) (b) (c) done

--- Unify.thy
+++ Unify.thy
@@ -28,9 +28,7 @@ proof (induction t)
   then show ?case by simp
 next
   case (Fun x1a x2)
-  then have "fv (\<sigma> · Fun x1a x2) = fv (Fun x1a (map (sapply \<sigma>) x2))" by simp
-  also have "... = fold (\<union>) (map fv (map (sapply \<sigma>) x2)) {}" by simp
-  also have "... = fold (\<union>) (map (fv \<circ> (sapply \<sigma>)) x2) {}" by simp
+  then have "fv (\<sigma> · Fun x1a x2) = fold (\<union>) (map (fv \<circ> (sapply \<sigma>)) x2) {}" by simp
   also have "... = (\<Union>y\<in>(set x2).((fv \<circ> (sapply \<sigma>)) y))" by (metis Sup_set_fold set_map)
   also have "... = (\<Union>y\<in>(set x2).(fv (\<sigma> · y)))" by simp
   also have "... =  (\<Union>y\<in>(set x2). (\<Union>yy\<in>fv y. fv (\<sigma> yy)))" using Fun.IH by simp
@@ -41,11 +39,74 @@ next
   then show ?case by simp
 qed
 
-lemma fv_sapply: "fv (\<sigma> · t) = (S
-x \<in> fv t. fv (\<sigma> x)) "
+lemma sapply_cong:
+  assumes "\<And>x. x \<in> fv t \<Longrightarrow> \<sigma> x = \<tau> x"
+  shows "\<sigma> · t = \<tau> · t"
+  using assms
+  apply (induction t)
+   apply auto
+  apply (metis (no_types, hide_lams) Sup_set_fold UnI1 Union_insert insert_absorb list.map(2) list.set(2) set_map)
+  done
+
+(*
+Tentative of Isar proof
+proof (induction t)
+  have "\<forall>x. \<sigma> · (Var x) = \<tau> · (Var x)"
+  proof (rule allI)
+    fix x
+    show "\<sigma> · (Var x) = \<tau> · (Var x)"
+    proof -
+      assume "t = Var x"
+      have "\<sigma> · (Var x) = \<sigma> x" by simp
+      moreover have "x \<in> fv (Var x)" by simp
+      also have "\<sigma> x = \<tau> x" using \<open>t = Var x\<close> assms by auto
+      also have "\<tau> x = \<tau>  · (Var x)" by simp
+      also have "\<sigma> · (Var x) =  \<tau>  · (Var x)"
+        using \<open>\<sigma> x = \<tau> x\<close> \<open>\<sigma> · Var x = \<sigma> x\<close> \<open>\<tau> x = \<tau> · Var x\<close> \<open>t = Var x\<close> by presburger
+      then show "\<sigma> · Var x = \<tau> · Var x" by simp
+next
+  case (Fun f l)
+  then show ?thesis sorry
+qed
+*)
 
 fun scomp :: "('f, 'v) subst \<Rightarrow> ('f, 'v) subst \<Rightarrow> ('f, 'v) subst" (infixl "\<circ>s" 75)
   where
-  "scomp s1 s2 = (\<lambda> x. s1 (s2 x))"
+  "scomp \<sigma> \<tau> = (\<lambda> x. \<sigma> · \<tau>(x))"
+
+lemma scomp_sapply: "(\<sigma> \<circ>s \<tau> ) x = \<sigma> ·(\<tau> x)"
+  by simp
+
+lemma sapply_scomp_distrib: "(\<sigma> \<circ>s \<tau> ) · t = \<sigma> · (\<tau> · t)"
+  apply (induction t)
+   apply simp_all
+  done
+
+lemma scomp_assoc: "(\<sigma> \<circ>s \<tau> ) \<circ>s q = \<sigma> \<circ>s (\<tau> \<circ>s q)"
+proof (rule ext)
+  show "(\<sigma> \<circ>s \<tau> \<circ>s q) x = (\<sigma> \<circ>s (\<tau> \<circ>s q)) x" for x
+    using sapply_scomp_distrib by simp
+qed
+
+lemma scomp_Var [simp]: "\<sigma> \<circ>s Var = \<sigma>"
+proof (rule ext)
+  show "(\<sigma> \<circ>s Var) x = \<sigma> x" for x
+    by simp
+qed
+
+thm term.induct
+
+lemma Var_id: "Var · t = t"
+proof (induction t)
+  case (Var x)
+then show ?case by simp
+next
+  case (Fun x1a x2)
+  then show ?case
+    by (simp add: map_idI)
+qed
+
+lemma Var_scomp [simp]: "Var \<circ>s \<sigma> = \<sigma>"
+  by (simp add: Var_id)
 
 end
